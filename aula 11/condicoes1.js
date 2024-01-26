5eee5615234f846a12bfec2ce7058c9e28280cb9
Hmarxx <henrymarx@hotmail.com>
2024-01-26 13:59:46 -0300

    aula 11

---
+++ aula 11/condicoes1.js
@@ -0,0 +1,11 @@
+console.log('console funcionando bem')
+
+var vel = 59.5;
+console.log(`use cinto de segurança`)
+
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade pemitida. MULTADO!`)
+} else {
+    console.log(`A velocidade do seu carro é ${vel} km/h`)
+    console.log(`Dentro do limite!`)
+}
---
+++ aula 11/condicoes2.js
@@ -0,0 +1,9 @@
+var pais = 'BRASIL';
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'BRASIL'){
+    console.log('Brasileiro(a)')
+}else {
+    console.log('Estrangeiro')
+}
